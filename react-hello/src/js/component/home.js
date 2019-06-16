2724aae5d054dbcd921967f56021b9cdf3ffc3bd
dcbounds <boundsdarius@gmail.com>
2019-06-16 20:09:24 +0000

    Added trafficlight.js

--- react-hello/src/js/component/home.js
+++ react-hello/src/js/component/home.js
@@ -2,24 +2,16 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import Trafficlight from "./trafficlight.js";
 
 //create your first component
 export default class Home extends React.Component {
 	render() {
 		return (
+            <div>
 			<div className="text-center mt-5">
-				<h1>Hello Rigo!</h1>
-				<p>
-					<img src={rigoImage} />
-				</p>
-				<a href="#" className="btn btn-success">
-					If you see this green button... bootstrap is working
-				</a>
-				<p>
-					Made by{" "}
-					<a href="http://www.4geeksacademy.com">4Geeks Academy</a>,
-					with love!
-				</p>
+                <Trafficlight />
+			</div>
 			</div>
 		);
 	}
---
+++ react-hello/src/js/component/trafficlight.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class Trafficlight extends React.Component {
+	render() {
+		return (
+			<div>
+            <div className="lightmain">
+            </div>
+
+			</div>
+		);
+	}
+}
